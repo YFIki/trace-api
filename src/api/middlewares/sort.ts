281b7c05d34a2890e24f4fc94c188ea8165bcb42
KanadeNishikawa <kanade.nishikawa@ikic.co.jp>
2020-05-01 13:51:28 +0900

    型を修正

--- src/api/middlewares/sort.ts
+++ src/api/middlewares/sort.ts
@@ -5,7 +5,7 @@
  * @param {string} 並び順（asc/desc）：値が正しくないor指定されていない場合はasc
  * @param {string} 配列に名前がついている場合指定
  */
-export const sortArrayObject = (array: Array<any>, key: string, sort?: string, arrayName?: string): Array<any> => {
+export const sortArrayObject = (array: Array<object>, key: string, sort?: string, arrayName?: string): Array<any> => {
   const target = array[arrayName] || array;
   target.sort(function(a, b) {
     if (a[key] > b[key]) {
