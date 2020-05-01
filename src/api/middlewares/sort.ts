11f28c2577acd0a2de1fade23404c18522c6696e
KanadeNishikawa <kanade.nishikawa@ikic.co.jp>
2020-05-01 13:48:31 +0900

    ソート関数とテストコード追加

---
+++ src/api/middlewares/sort.ts
@@ -0,0 +1,19 @@
+/**
+ * 引数で指定された配列を指定された通りにソートする
+ * @param {Array} 対象配列
+ * @param {string} ソートするキー
+ * @param {string} 並び順（asc/desc）：値が正しくないor指定されていない場合はasc
+ * @param {string} 配列に名前がついている場合指定
+ */
+export const sortArrayObject = (array: Array<any>, key: string, sort?: string, arrayName?: string): Array<any> => {
+  const target = array[arrayName] || array;
+  target.sort(function(a, b) {
+    if (a[key] > b[key]) {
+      return sort === 'desc' ? -1 : 1;
+    } else {
+      return sort === 'desc' ? 1 : -1;
+    }
+  });
+
+  return array;
+}
