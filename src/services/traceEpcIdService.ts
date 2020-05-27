f439f0702e2181ef6b6233a361c001cbf8307de0
KanadeNishikawa <kanade.nishikawa@ikic.co.jp>
2020-05-27 11:23:53 +0900

    不要なconsole.logを削除

--- src/services/traceEpcIdService.ts
+++ src/services/traceEpcIdService.ts
@@ -103,7 +103,6 @@ export const getTraceConsumer = async (
     // 取得されたeventsに上記で取得したpayloadsを追加したJSONを返却する
     return {events: sortArrayObject(events, 'dateTime', 'desc'), payloads: payloads};
   } catch (err) {
-    console.log(err);
     throw err;
   }
 }
